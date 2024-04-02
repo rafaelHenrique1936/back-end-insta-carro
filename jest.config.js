4c0069ae95a248d14a1b5d21cab07939c1c6f101
Rafael Henrique Alves Pereira <rafaelalves1936@gmail.com>
2024-04-02 08:32:48 -0300

    Arquivos de Configuração

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+  };
