588020f763eb33046bb3515f9008b7fefc9ea0ae
Rafael Henrique Alves Pereira <rafaelalves1936@gmail.com>
2024-04-02 08:37:27 -0300

    Autenticação, conexão com os Bancos e arquivo de startUp do projeto

---
+++ infra/db.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+
+class DataBase{
+    private DB_URL = "mongodb://localhost:27017/db_insta_carro";
+    //private DB_URL = "mongodb://link-db/db_insta_carro";
+
+    createConnection(){
+        mongoose.connect(this.DB_URL).then(() => console.log("conectou no banco de dados"));
+        
+    }
+}
+
+export default DataBase;
